33bb89568d0300dd3c124bdf9cfb84468492b5fc
Denis <soldugeev.d@cronix.ms>
2020-08-29 15:45:23 +0300

    update_gulp

--- src/js/script.js
+++ src/js/script.js
@@ -3,18 +3,37 @@ function testWebP(callback) {
 
   var webP = new Image();
   webP.onload = webP.onerror = function () {
-  callback(webP.height == 2);
+    callback(webP.height == 2);
   };
   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
-  
-  testWebP(function (support) {
-  
-  if (support == true) {
-  document.querySelector('body').classList.add('webp');
-  }else{
-  document.querySelector('body').classList.add('no-webp');
-  }
-  });
+}
 
-  // end  Проверка поддержки браузера формата изображения webP
+testWebP(function (support) {
+
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});
+// end  Проверка поддержки браузера формата изображения webP
+
+// <Делает картинку фоном>
+function ibg() {
+
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+  }
+}
+ibg();
+// </Делает картинку фоном>
+
+// <Добавление класса active>
+$('.icon-menu').click(function(event) {
+  $(this).toggleClass('active');
+  $('.menu__body').toggleClass('active');
+})
+// </Добавление класса active>
